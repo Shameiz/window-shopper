d465b841a5001eef600af1f31b4c58167fb0c4b5
Shameiz Rangwala <srangwal@edu.uwaterloo.ca>
2018-02-07 16:44:59 -0500

    first commit

---
+++ window-shopper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Shameiz Rangwala on 2018-01-25.
+//  Copyright © 2018 CodeSchool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
