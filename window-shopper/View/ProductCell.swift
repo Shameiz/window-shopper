be76103a5791b99cee20fa1fe77cedc89d7a66cf
Shameiz Rangwala <srangwal@edu.uwaterloo.ca>
2018-02-12 00:15:29 -0500

    added collection view UI

---
+++ window-shopper/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  window-shopper
+//
+//  Created by Shameiz Rangwala on 2018-02-12.
+//  Copyright © 2018 CodeSchool. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage : UIImageView!
+    @IBOutlet weak var productTitle : UILabel!
+    @IBOutlet weak var productPrice : UILabel!
+    
+}
